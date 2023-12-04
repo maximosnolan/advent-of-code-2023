b60f80bf1c513db8518e2c712018c7aca41c069f
maximosnolan <maximosnolan@gmail.com>
2023-12-04 01:31:35 -0500

    added both solutions

--- day4/src/main/java/com/day4/main.java
+++ day4/src/main/java/com/day4/main.java
@@ -2,7 +2,9 @@ package src.main.java.com.day4;
 
 public class main {
     public static void main(String[] args) {
-        int part1Solution = part2.solvePart2();
-        System.out.println("Part 2: " + part1Solution);
+        int solutionPart1 = part1.solvepart1();
+        int solutionPart2 = part2.solvepart2();
+        System.out.println("Part 1: " + solutionPart1);
+        System.out.println("Part 2: " + solutionPart2);
     }
 }
